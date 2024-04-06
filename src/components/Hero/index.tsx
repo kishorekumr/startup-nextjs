5fca3f2893547c4790ee1db50b23239ba92ef7d0
kishorekumr <kishore.saiprakash@gmail.com>
2024-04-06 10:49:26 +0530

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,13 +12,13 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  One stop solution to integrate all your strategies across Tradingview, Telegram and more
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                  With vast experience in integrating various brokers 
+                  we have a suite of products to expedite your algo development.
+                  We provide multi trading terminal to trade across any broker
+                  and also develop your own strategy to be deployed for multiple clients.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
 {/*                   <Link
