3497733d065da3178017b7f97e9bf81c342f8f4c
kishorekumr <kishore.saiprakash@gmail.com>
2024-04-06 10:54:40 +0530

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-center">
+              <div className="mx-auto max-w-[1200px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  One stop solution to integrate all your strategies across Tradingview, Telegram and more
+                  One stop solution for automating trading strategies
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   With vast experience in integrating various brokers 
