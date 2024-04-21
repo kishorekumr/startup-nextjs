bf95cc85ceb103285ee6eae7d16aa482ad994e64
kishorekumr <kishore.saiprakash@gmail.com>
2024-04-21 18:28:55 +0530

    Update index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -11,7 +11,7 @@ const Blog = () => {
       <div className="container">
         <SectionTitle
           title="Our Latest Blogs"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          paragraph="We cater to variety of tools for retail traders and for sub-brokers as well."
           center
         />
 
