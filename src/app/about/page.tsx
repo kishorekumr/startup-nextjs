45af67743ed6666f288743cce24efa2ead8f3398
kishorekumr <kishore.saiprakash@gmail.com>
2024-06-02 15:45:24 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
+  title: "About Page | Amazealgos",
+  description: "This is About Page for Amazealgos",
   // other metadata
 };
 
@@ -15,7 +15,7 @@ const AboutPage = () => {
     <>
       <Breadcrumb
         pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        description="We are providers of robust algos for trading setups and automation for scheduled tasks  ."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
